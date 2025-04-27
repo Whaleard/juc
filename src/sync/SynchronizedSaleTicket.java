da18fdbd2cf6b51eef409741e95b560be3737816
chengmengd <chengmengd@isoftstone.com>
2025-04-27 11:26:12 +0800

    synchronized关键字

--- src/sync/SynchronizedSaleTicket.java
+++ src/sync/SynchronizedSaleTicket.java
@@ -4,10 +4,10 @@ package sync;
 class Ticket {
 
     // 票数
-    int number = 30;
+    private int number = 30;
 
     // 卖票方法
-    synchronized void sale() {
+    public synchronized void sale() {
         // 判断：是否有票
         if (number > 0) {
             System.out.println(Thread.currentThread().getName() + "：卖出第" + (number--) + "张票，剩下：" + number + "张票");
