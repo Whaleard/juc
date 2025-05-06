b5308f3fa449256b067d049c220700c81e37d097
chengmengd <chengmengd@isoftstone.com>
2025-05-06 11:12:51 +0800

    Semaphore信号量

---
+++ src/unit/SemaphoreTest.java
@@ -0,0 +1,41 @@
+package unit;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 6辆汽车停3个停车位
+ */
+public class SemaphoreTest {
+
+    public static void main(String[] args) {
+        // 创建Semaphore，设置许可数量
+        Semaphore semaphore = new Semaphore(3);
+
+        // 模拟6辆汽车
+        for (int i = 1; i <= 6; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // 抢占
+                    try {
+                        semaphore.acquire();
+
+                        System.out.println(Thread.currentThread().getName() + "抢到了车位");
+
+                        // 设置随机停车时间
+                        TimeUnit.SECONDS.sleep(new Random().nextInt(5));
+
+                        System.out.println(Thread.currentThread().getName() + "离开了车位");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        // 释放
+                        semaphore.release();
+                    }
+                }
+            }, String.valueOf(i)).start();
+        }
+    }
+}
