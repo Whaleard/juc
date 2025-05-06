54528b6c3cceeb663bb644972e1ee9664b08eb57
chengmengd <chengmengd@isoftstone.com>
2025-05-06 11:13:24 +0800

    CountDownLatch实现集齐七龙珠问题

--- src/unit/CyclicBarrierTest.java
+++ src/unit/CyclicBarrierTest.java
@@ -3,6 +3,7 @@ package unit;
 import org.junit.Test;
 
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
 /**
@@ -13,7 +14,11 @@ public class CyclicBarrierTest {
     // 创建固定值
     private static final int NUMBER = 7;
 
-    public static void main(String[] args) {
+    /**
+     * 通过CyclicBarrier实现
+     */
+    @Test
+    public void test01() {
         // 创建CyclicBarrier
         CyclicBarrier cyclicBarrier = new CyclicBarrier(NUMBER, new Runnable() {
             @Override
@@ -22,7 +27,7 @@ public class CyclicBarrierTest {
             }
         });
 
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= NUMBER; i++) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -37,5 +42,30 @@ public class CyclicBarrierTest {
                 }
             }, String.valueOf(i)).start();
         }
+    }
+
+    /**
+     * 通过CountDownLatch实现
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void test02() throws InterruptedException {
+
+        CountDownLatch countDownLatch = new CountDownLatch(NUMBER);
+
+        for (int i = 1; i <= NUMBER; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "星龙珠已找到");
+
+                    countDownLatch.countDown();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+        countDownLatch.await();
+        System.out.println("============集齐7颗龙珠就可以召唤神龙============");
     }
 }
