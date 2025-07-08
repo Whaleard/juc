40f0083cdb0d6606e1bc9c7556e6788e64f809e3
chengmengd <chengmengd@isoftstone.com>
2025-07-08 15:45:57 +0800

    集合线程安全问题

--- src/lock/CollectionSafetyIssues.java
+++ src/lock/CollectionSafetyIssues.java
@@ -13,6 +13,64 @@ import java.util.concurrent.CopyOnWriteArraySet;
 public class CollectionSafetyIssues {
 
     /**
+     * ArrayList的add()方法没有加锁
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void test01() throws InterruptedException {
+        // 创建ArrayList集合
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                // 向集合中添加内容
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                // 从集合中获取内容
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+
+        Thread.sleep(3000L);
+    }
+
+    /**
+     * Vector的add()方法在方法声明时加上了synchronized关键字
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void test02() throws InterruptedException {
+        // 创建ArrayList集合
+        List<String> list = new Vector<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                // 向集合中添加内容
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                // 从集合中获取内容
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+
+        Thread.sleep(3000L);
+    }
+
+    @Test
+    public void test03() throws InterruptedException {
+        // 创建ArrayList集合
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                // 向集合中添加内容
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                // 从集合中获取内容
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+
+        Thread.sleep(3000L);
+    }
+
+    /**
      * ArrayList线程不安全，若多个线程同时对ArrayList进行读写操作，会出现ConcurrentModificationException异常
      * 解决办法：
      *      1、使用Vector替换ArrayList
@@ -22,17 +80,30 @@ public class CollectionSafetyIssues {
      * @throws InterruptedException
      */
     @Test
-    public void test01() throws InterruptedException {
-
+    public void test04() throws InterruptedException {
         List<String> list = new CopyOnWriteArrayList<>();
-
         for (int i = 0; i < 30; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    list.add(UUID.randomUUID().toString().substring(0, 8));
-                    System.out.println(list);
-                }
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+
+        Thread.sleep(3000L);
+    }
+
+    /**
+     * HashSet的add()方法没有加锁
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void test05() throws InterruptedException {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                set.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set);
             }, String.valueOf(i)).start();
         }
 
@@ -45,15 +116,12 @@ public class CollectionSafetyIssues {
      * @throws InterruptedException
      */
     @Test
-    public void test02() throws InterruptedException {
+    public void test06() throws InterruptedException {
         Set<String> set = new CopyOnWriteArraySet<>();
         for (int i = 0; i < 30; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    set.add(UUID.randomUUID().toString().substring(0, 8));
-                    System.out.println(set);
-                }
+            new Thread(() -> {
+                set.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set);
             }, String.valueOf(i)).start();
         }
 
@@ -61,21 +129,37 @@ public class CollectionSafetyIssues {
     }
 
     /**
-     * HashMap线程不安全，使用ConcurrentHashMap替换HashSet
+     * HashMap的put()方法没有加锁
      *
      * @throws InterruptedException
      */
     @Test
-    public void test03() throws InterruptedException {
+    public void test07() throws InterruptedException {
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < 30; i++) {
+            String key = String.valueOf(i);
+            new Thread(() -> {
+                map.put(key, UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+        }
+
+        Thread.sleep(3000L);
+    }
+
+    /**
+     * HashMap线程不安全，使用ConcurrentHashMap替换HashMap
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void test08() throws InterruptedException {
         Map<String, String> set = new ConcurrentHashMap<>();
         for (int i = 0; i < 30; i++) {
             String key = String.valueOf(i);
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    set.put(key, UUID.randomUUID().toString().substring(0, 8));
-                    System.out.println(set);
-                }
+            new Thread(() -> {
+                set.put(key, UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set);
             }, String.valueOf(i)).start();
         }
 
