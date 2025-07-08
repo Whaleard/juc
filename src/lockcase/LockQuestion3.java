a180a9d0f6f2c2a366716fbf356063a1988d4c21
chengmengd <chengmengd@isoftstone.com>
2025-07-08 17:27:49 +0800

    两个线程通过相同实例对象访问同步实例方法和普通实例方法不会产生阻塞问题

---
+++ src/lockcase/LockQuestion3.java
@@ -0,0 +1,47 @@
+package lockcase;
+
+import java.util.concurrent.TimeUnit;
+
+class Phone3 {
+
+    public synchronized void sendSMS() {
+        try {
+            TimeUnit.SECONDS.sleep(3L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("============sendSMS============");
+    }
+
+    public void hello() {
+        System.out.println("============hello============");
+    }
+}
+
+/**
+ * 问题三：新增普通的hello()方法，先打短信还是hello
+ * ============hello============
+ * ============sendSMS============
+ *
+ * 使用synchronized修饰的方法与不使用synchronized修饰的方法不会产生冲突
+ */
+public class LockQuestion3 {
+
+    public static void main(String[] args) {
+        Phone3 phone = new Phone3();
+
+        new Thread(() -> {
+            phone.sendSMS();
+        }, "A").start();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(200L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            phone.hello();
+        }, "B").start();
+    }
+}
