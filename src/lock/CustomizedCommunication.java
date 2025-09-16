d777cf37504db9ab144180adc0f6488b66ccde8b
chengmengd <chengmengd@isoftstone.com>
2025-09-16 11:48:30 +0800

    线程间定制化通信优化

--- src/lock/CustomizedCommunication.java
+++ src/lock/CustomizedCommunication.java
@@ -4,112 +4,120 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-// 第一步：创建资源类
+/**
+ * 第一步：创建资源类
+ */
 class ShareResource {
-    // 定义标志位
+    /**
+     * 定义标志位
+     */
     private int flag = 1;
 
-    // 创建Lock锁
+    /**
+     * 创建Lock锁
+     */
     private Lock lock = new ReentrantLock();
 
-    private Condition c1 = lock.newCondition();
-    private Condition c2 = lock.newCondition();
-    private Condition c3 = lock.newCondition();
+    private Condition c1 = this.lock.newCondition();
+    private Condition c2 = this.lock.newCondition();
+    private Condition c3 = this.lock.newCondition();
 
     public void printA(int loop) throws InterruptedException {
         // 上锁
-        lock.lock();
+        this.lock.lock();
         try {
-            while (flag != 1) {
-                c1.await();
+            while (this.flag != 1) {
+                // 等待
+                this.c1.await();
             }
             for (int i = 1; i <= 5; i++) {
                 System.out.println(Thread.currentThread().getName() + "::" + i + "：轮数：" + loop);
             }
-            flag = 2;
-            c2.signal();
+            this.flag = 2;
+            // 唤醒
+            this.c2.signal();
         } finally {
             // 释放锁
-            lock.unlock();
+            this.lock.unlock();
         }
     }
 
     public void printB(int loop) throws InterruptedException {
         // 上锁
-        lock.lock();
+        this.lock.lock();
         try {
-            while (flag != 2) {
-                c2.await();
+            while (this.flag != 2) {
+                // 等待
+                this.c2.await();
             }
             for (int i = 1; i <= 10; i++) {
                 System.out.println(Thread.currentThread().getName() + "::" + i + "：轮数：" + loop);
             }
-            flag = 3;
-            c3.signal();
+            this.flag = 3;
+            // 唤醒
+            this.c3.signal();
         } finally {
             // 释放锁
-            lock.unlock();
+            this.lock.unlock();
         }
     }
 
     public void printC(int loop) throws InterruptedException {
         // 上锁
-        lock.lock();
+        this.lock.lock();
         try {
-            while (flag != 3) {
-                c3.await();
+            while (this.flag != 3) {
+                // 等待
+                this.c3.await();
             }
             for (int i = 1; i <= 15; i++) {
                 System.out.println(Thread.currentThread().getName() + "::" + i + "：轮数：" + loop);
             }
-            flag = 1;
-            c1.signal();
+            this.flag = 1;
+            // 唤醒
+            this.c1.signal();
         } finally {
             // 释放锁
-            lock.unlock();
+            this.lock.unlock();
         }
     }
 }
 
+/**
+ * 多线程定制化通信
+ *
+ * @author Mr.MC
+ */
 public class CustomizedCommunication {
 
     public static void main(String[] args) {
         ShareResource shareResource = new ShareResource();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 1; i <= 10; i++) {
-                    try {
-                        shareResource.printA(i);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                try {
+                    shareResource.printA(i);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }, "A").start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 1; i <= 10; i++) {
-                    try {
-                        shareResource.printB(i);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                try {
+                    shareResource.printB(i);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }, "B").start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 1; i <= 10; i++) {
-                    try {
-                        shareResource.printC(i);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                try {
+                    shareResource.printC(i);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }, "C").start();
