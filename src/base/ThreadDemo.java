8d344f0e1de1c8e49261f37441ba4cf29e047cff
chengmengd <chengmengd@isoftstone.com>
2025-09-17 11:25:49 +0800

    线程创建的四种方式注释优化

--- src/base/ThreadDemo.java
+++ src/base/ThreadDemo.java
@@ -5,17 +5,22 @@ import java.util.concurrent.FutureTask;
 
 /**
  * 线程创建的四种方式
- *      1、继承Thread类，调用run()方法，无返回值
+ *      1、继承Thread类，调用start()方法，无返回值
  *      2、实现Runnable接口，调用start()方法，无返回值
  *      3、实现Callable接口，有返回值
  *      4、使用线程池
+ *
+ * 注意：1、2、3三种方式最后都是通过Thread的start()方法来开启线程调用
+ *      调用start()方法会新建一个线程，而调用run()方法只有主线程执行
+ *
+ * @author Mr.MC
  */
 public class ThreadDemo {
 
     public static void main(String[] args) {
         // 继承Thread类
         MyThread t = new MyThread();
-        t.run();
+        t.start();
 
         // 实现Runnable接口
         MyRunnable mr = new MyRunnable();
