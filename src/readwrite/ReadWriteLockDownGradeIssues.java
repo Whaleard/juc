396697c45cbb7a189a0d168da46e755bed5b7319
chengmengd <chengmengd@isoftstone.com>
2025-09-18 21:59:51 +0800

    锁降级问题优化

--- src/readwrite/ReadWriteLockDownGradeIssues.java
+++ src/readwrite/ReadWriteLockDownGradeIssues.java
@@ -1,13 +1,26 @@
 package readwrite;
 
+import org.junit.Test;
+
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * 锁降级问题
+ *
+ * @author Mr.MC
+ */
 public class ReadWriteLockDownGradeIssues {
 
-    public static void main(String[] args) {
+    /**
+     * 写锁可以降级为读锁
+     */
+    @Test
+    public void test01() {
         // 可重入读写锁对象
         ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+        // 读锁
         ReentrantReadWriteLock.ReadLock readLock = rwLock.readLock();
+        // 写锁
         ReentrantReadWriteLock.WriteLock writeLock = rwLock.writeLock();
 
         // 锁降级
@@ -24,5 +37,33 @@ public class ReadWriteLockDownGradeIssues {
 
         // 4、释放读锁
         readLock.unlock();
+    }
+
+    /**
+     * 读锁不可以升级为写锁
+     */
+    @Test
+    public void test02() {
+        // 可重入读写锁对象
+        ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+        // 读锁
+        ReentrantReadWriteLock.ReadLock readLock = rwLock.readLock();
+        // 写锁
+        ReentrantReadWriteLock.WriteLock writeLock = rwLock.writeLock();
+
+        // 锁升级
+        // 1、获取读锁
+        readLock.lock();
+        System.out.println("执行读操作");
+
+        // 2、获取写锁
+        writeLock.lock();
+        System.out.println("执行写操作");
+
+        // 3、释放读锁
+        readLock.unlock();
+
+        // 4、释放写锁
+        writeLock.unlock();
     }
 }
